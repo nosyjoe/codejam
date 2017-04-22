9b25aed416aa3826e53c64e4578f1757396cd719
Philipp Engel <philipp@filzip.com>
2017-04-22 11:59:47 +0200

    correctly renames test input, creates sub-rounds (a,b,c,...)

--- gen.rb
+++ gen.rb
@@ -5,6 +5,7 @@ require 'fileutils'
 def copy_files(folder)
   ["a", "b", "c", "d"].each do |problem|
     FileUtils.cp_r "template", "#{folder}/#{problem}", :verbose => false
+    File.rename("#{folder}/#{problem}/A-test.in", "#{folder}/#{problem}/#{problem.upcase}-test.in")
   end
 end
 
@@ -16,9 +17,10 @@ case what
 when /q|qualification/
   puts "Generating qualification for year #{year}"
   target = 'qualification'
-when /r(\d)|round(\d)/
-  puts "Generating round#{$1} for year #{year}"
-  target = "round#{$1}"
+when /r(\d)(\w)?|round(\d)(\w)?/
+  roundname = $1 + ($2 ? $2 : "")
+  puts "Generating round#{roundname} for year #{year}"
+  target = "round#{roundname}"
 else
   puts "Don't know how to generate #{what}"
 end
